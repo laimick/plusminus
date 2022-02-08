0b60e2f70f4023ff8dbdd50a73609d9a8bc0ca7f
laimick <kirill.perih.2003@gmail.com>
2022-02-08 11:35:40 +0500

    start

---
+++ chose/chose/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace chose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ввести А");
+           a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ввести В");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите операцию(+,-,*,/.");
+            switch (Console.ReadLine())
+            {
+                case "+":
+                {
+                    Console.WriteLine("- выполнить операцию “+”");
+                    Console.WriteLine(a + b);
+                    break;
+                }
+                case "-":
+                    {
+                        Console.WriteLine("- выполнить операцию “-”");
+                        Console.WriteLine(a - b);
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine("- выполнить операцию “*”");
+                        Console.WriteLine(a * b);
+                        break;
+                    }
+                case "/":
+                    {
+                        Console.WriteLine("- выполнить операцию “/”");
+                        Console.WriteLine(a/ b);
+                        break;
+                    }
+            }
+        }
+    }
+}
