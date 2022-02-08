398935772a8149d05aab5e921cddce0c76e95d21
laimick <kirill.perih.2003@gmail.com>
2022-02-08 11:39:35 +0500

    A

--- chose/chose/Program.cs
+++ chose/chose/Program.cs
@@ -9,35 +9,6 @@ namespace chose
             int a, b;
             Console.WriteLine("Ввести А");
            a=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Ввести В");
-            b = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите операцию(+,-,*,/.");
-            switch (Console.ReadLine())
-            {
-                case "+":
-                {
-                    Console.WriteLine("- выполнить операцию “+”");
-                    Console.WriteLine(a + b);
-                    break;
-                }
-                case "-":
-                    {
-                        Console.WriteLine("- выполнить операцию “-”");
-                        Console.WriteLine(a - b);
-                        break;
-                    }
-                case "*":
-                    {
-                        Console.WriteLine("- выполнить операцию “*”");
-                        Console.WriteLine(a * b);
-                        break;
-                    }
-                case "/":
-                    {
-                        Console.WriteLine("- выполнить операцию “/”");
-                        Console.WriteLine(a/ b);
-                        break;
-                    }
             }
         }
     }
